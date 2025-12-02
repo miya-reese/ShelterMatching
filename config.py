c5c06d405bf27a078fc6dd31b29958943b98d830
miya-reese <81585316+miya-reese@users.noreply.github.com>
2025-12-02 12:43:36 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Input your Smartsheet access token here
+# SMARTSHEET_ACCESS_TOKEN = ""
+
+# Input your Smartsheet sheet IDs here
+'''
+SMARTSHEET_CONFIG = {
+    "General_Shelter_Referral_Forms": ,
+    "Shelter_Bed_Availability": ,
+    "Shelter_Database": 
+}
+'''
